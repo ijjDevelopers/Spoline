a958b6df76f8612f9f2cafd25cca64df961d15a0
Ivan <ivan.cozar90@gmail.com>
2016-03-04 00:50:51 +0100

    Insert en base de datos y login

--- app/src/main/java/com/studio/ijj/spoline/LoginActivity.java
+++ app/src/main/java/com/studio/ijj/spoline/LoginActivity.java
@@ -3,9 +3,11 @@ package com.studio.ijj.spoline;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -19,6 +21,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -28,9 +31,23 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.studio.ijj.spoline.Model.Usuario;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,16 +59,13 @@ import static android.Manifest.permission.READ_CONTACTS;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     private Usuario usuario;
+    private static final String URL="http://helpmenow.esy.es/login.php";
 
     /**
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
@@ -110,7 +124,55 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
+    public String makePOSTRequestRegister(String url, Usuario user) {
+        String response = "";
 
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            // add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("usuario", user.getNombre()));
+            nameValuePairs.add(new BasicNameValuePair("contrasena", user.getPassword()));
+            nameValuePairs.add(new BasicNameValuePair("email", user.getEmail()));
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            response = EntityUtils.toString(httpEntity);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //Log.d(LOGTAG, "POST Response >>> " + response);
+        return response;
+
+    }
+    private class LogOn extends AsyncTask<Usuario, Void, String> {
+
+        private Exception exception;
+
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            Toast.makeText(LoginActivity.this, response,
+                    Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected String doInBackground(Usuario... params) {
+
+            String response = makePOSTRequestRegister(URL, usuario);
+            /*Toast.makeText(LoginActivity.this, response,
+                    Toast.LENGTH_LONG).show();*/
+            Log.i("LocAndroid", String.valueOf(usuario.getNombre() + " - " + String.valueOf(usuario.getEmail())));
+
+            return response;
+        }
+    }
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
             return;
@@ -214,25 +276,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password, username);
             mAuthTask.execute((Void) null);
-            //Camera activity
-            //View v= new View(this);
-            //mandarCorreo(v,email);
 
             /**********************************************/
 
             /*USUARIO*/
-           /* usuario = new Usuario();
+
+            usuario.setEmail(email);
+            usuario.setNombre(username);
+            usuario.setPassword(password);
             Intent returnIntent = new Intent();
-            returnIntent.putExtra("Usuario",usuario.getUsuario());
+            returnIntent.putExtra("Usuario",usuario.getNombre());
             returnIntent.putExtra("Email", usuario.getEmail());
             returnIntent.putExtra("Password", usuario.getPassword());
             new LogOn().execute();
             setResult(Activity.RESULT_OK,returnIntent);
-            finish();*/
-            /*Intent cam = new Intent(LoginActivity.this, EmailActivity.class);
-            startActivity(cam);*/
-            /*Intent cam = new Intent(LoginActivity.this, CameraActivity.class);
-            startActivity(cam);*/
+            finish();
         }
     }
 
